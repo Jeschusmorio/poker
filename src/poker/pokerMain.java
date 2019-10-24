6136fc97e6f2ed56153058c8f8a450b0ab88906c
Jeschusmorio <mpetrovic1@tsn.at>
2019-10-24 08:11:31 +0200

    added input

--- src/poker/pokerMain.java
+++ src/poker/pokerMain.java
@@ -1,7 +1,9 @@
 package poker;
 
 import java.util.Arrays;
-import java.util.Random;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+//import java.util.Random;
 
 public class pokerMain {
 
@@ -10,170 +12,224 @@ public class pokerMain {
 		for (int i = 0; i < wslZaehler.length; i++) {
 			wslZaehler[i] = 0;
 		}
-		for (int i = 0; i < ANZAHL_DURCHLAEUFE; i++) {
-			kartendeck(52);
-			ziehung(5);
-			//String ausgabe="High Card";
-			if (checkPair(1)) {wslZaehler[1]++;} 					//One Pair
-			if (twoPair()) {wslZaehler[2]++;} 						//Two Pair;
-			if (checkPair(2)) {wslZaehler[3]++;}					//Three of a Kind
-			if (straight()) {wslZaehler[4]++;}						//Straight
-			if (flush()) {wslZaehler[5]++;}							//Flush
-			if (checkPair(2) && checkPair(1)) {wslZaehler[6]++;} 	//Full House
-			if (checkPair(3)) {wslZaehler[7]++;} 					//Four of a Kind
-			if (straight() && flush()) {wslZaehler[8]++;}			//Straight Flush
-			if (royalFlush()) {wslZaehler[9]++;} 					//Royal Flush
+		anzahlDurchlaeufe = input();
+		for (int i = 0; i < anzahlDurchlaeufe; i++) {
+			kartendeck(kANZAHL);
+			ziehung(hANZAHL);
+			boolean highCardCheck = true;
+			if (checkPair(1)) {						//One Pair
+				wslZaehler[1]++;
+				highCardCheck = false;
+			}
+			if (checkPair(2)) {						//Two Pair
+				wslZaehler[2]++;
+				highCardCheck = false;
+			}
+			if (checkPair(3)) {						//Three of a Kind
+				wslZaehler[3]++;
+				highCardCheck = false;
+			}
+			if (straight()) {						//Straight
+				wslZaehler[4]++;
+				highCardCheck = false;
+			}
+			if (flush()) {							//Flush
+				wslZaehler[5]++;
+				highCardCheck = false;
+			}
+			if (checkPair(4)) {						//Full House
+				wslZaehler[6]++;
+				highCardCheck = false;
+			}
+			if (checkPair(6)) {						//Four of a Kind
+				wslZaehler[7]++;
+				highCardCheck = false;
+			}
+			if (straight() && flush()) {			//Straight Flush
+				wslZaehler[8]++;
+				highCardCheck = false;
+			}
+			if (royalFlush()) {						//Royal Flush
+				wslZaehler[9]++;
+				highCardCheck = false;
+			}
+			if (highCardCheck) {					//High Card
+				wslZaehler[0]++;
+			}
 			//System.out.println(ausgabe);
 		}
-		float sumZaehler=0;
-		for (int i = 1; i < ANZAHL_KOMBINATIONEN; i++) {
-			sumZaehler= sumZaehler + wslZaehler[i];
+		for (int i = 0; i < ANZAHL_KOMBINATIONEN; i++) {
+			sumZaehler = sumZaehler + wslZaehler[i];
 		}
-		wslZaehler[0] = ANZAHL_DURCHLAEUFE - sumZaehler;			//High Card
-		wslAusgabe("High Card", wslZaehler[0]);
-		wslAusgabe("One Pair", wslZaehler[1]);
-		wslAusgabe("Two Pair", wslZaehler[2]);
-		wslAusgabe("Three of a Kind", wslZaehler[3]);
-		wslAusgabe("Straight", wslZaehler[4]);
-		wslAusgabe("Flush", wslZaehler[5]);
-		wslAusgabe("Full House", wslZaehler[6]);
-		wslAusgabe("Four of a Kind", wslZaehler[7]);
-		wslAusgabe("Straight Flush", wslZaehler[8]);
-		wslAusgabe("Royal Flush", wslZaehler[9]);
+		wsl("High Card", wslZaehler[0], "50,12");
+		wsl("One Pair", wslZaehler[1], "42,26");
+		wsl("Two Pair", wslZaehler[2], "4,75");
+		wsl("Three of a Kind", wslZaehler[3], "2,11");
+		wsl("Straight", wslZaehler[4], "0,392");
+		wsl("Flush", wslZaehler[5], "0,197");
+		wsl("Full House", wslZaehler[6], "0,144");
+		wsl("Four of a Kind", wslZaehler[7], "0,024");
+		wsl("Straight Flush", wslZaehler[8], "0,00139");
+		wsl("Royal Flush", wslZaehler[9], "0,000154");
 	}
+	
+	//VARIABLEN:
+	
 	static int[] kartendeck;
-	static int kANZAHL;
-	static final int ANZAHL_KARTEN_PRO_FARBE=13;
+	static final int kANZAHL = 52;
+	static final int ANZAHL_KARTEN_PRO_FARBE = 13;
 	static int[] hand;
-	static int hANZAHL;
-	static final int ANZAHL_DURCHLAEUFE=1000000;
-	static final int ANZAHL_KOMBINATIONEN=10;
+	static final int hANZAHL = 5;
+	//static final int ANZAHL_DURCHLAEUFE = 1000000;
+	static long anzahlDurchlaeufe;
+	static final int ANZAHL_KOMBINATIONEN = 10;
+	static float sumZaehler = 0;
 
+	//HILFSMETHODEN:
+	
+	public static long input() {
+		long input = 0;
+		boolean inputOk;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Geben Sie die Anzahl der Durchläufe ein: ");
+			try {
+				input = sc.nextInt();
+				inputOk = true;
+				if (input < 1) {
+					System.out.println("Sie müssen mindestens einen Durchlauf ausführen!");
+					inputOk = false;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Geben Sie bitte eine Zahl ein!");
+				inputOk = false;
+			}
+		} while (!inputOk);
+		return input;
+	}
+	public static int farbe(int karte) {
+		return karte / ANZAHL_KARTEN_PRO_FARBE;
+	}
+	public static int kartensymbolik(int karte) {
+		return karte % ANZAHL_KARTEN_PRO_FARBE;
+	}
+	
+	//DECK & HAND:
+	
 	public static void kartendeck(int LAENGE) {
-		kANZAHL=LAENGE;
-		kartendeck=new int[kANZAHL];
-		for (int i=0;i<kANZAHL;i++) {
-			kartendeck[i]=i;
+		kartendeck = new int[LAENGE];
+		for (int i = 0; i < LAENGE; i++) {
+			kartendeck[i] = i;
 		}
 	}
-	public static void ziehung(int gezogeneKarten) {
-		hANZAHL=gezogeneKarten;
-		Random zufall=new Random();
-		hand=new int[hANZAHL];
-		int moeglKarten=kANZAHL;
-		for (int i=0;i<hANZAHL;i++) {
-			int z=zufall.nextInt(kANZAHL);
-			int[] ph=kartendeck;
-			hand[i]=ph[z];
-			ph[z]=kartendeck[moeglKarten-1];
-			ph[moeglKarten-1]=kartendeck[z];
-			moeglKarten--;
+	public static void ziehung(int hANZAHL) {
+		//Random zufall = new Random();
+		hand = new int[hANZAHL];
+		int letzteKarte = kANZAHL - 1;
+		int[] kartendeckKopie = kartendeck;
+		/*for (int i = 0; i < kANZAHL; i++) {
+			System.out.print(kartendeckKopie[i] + " ");
+		}
+		System.out.println();*/
+		for (int i = 0; i < hANZAHL; i++) {
+			int z = (int)(Math.random() * kANZAHL);
+			hand[i] = kartendeckKopie[z];
+			kartendeckKopie[z] = kartendeck[letzteKarte];
+			kartendeckKopie[letzteKarte] = kartendeck[z];
+			letzteKarte--;
 		}
 		/*System.out.print("Gezogene Karten : ");
-		for (int i=0;i<hANZAHL;i++) {
-			System.out.print(hand[i]+" ");
+		for (int i = 0; i < hANZAHL; i++) {
+			System.out.print(kartensymbolik(hand[i]) + " ");
 		}
 		System.out.println();*/
 	}
-	public static int farbe(int karte) {
-		return karte/ANZAHL_KARTEN_PRO_FARBE;
-	}
-	public static int kartensymbolik(int karte) {
-		return karte%ANZAHL_KARTEN_PRO_FARBE;
-	}
-	public static boolean checkPair(int gleiche) {
-		boolean pair=false;
-		int zaehler=0;
-		for (int i=0;i<hANZAHL;i++) {
-			for (int j=(i+1);j<hANZAHL;j++) {
-				if (kartensymbolik(hand[i])==kartensymbolik(hand[j])) {
-					zaehler++;
-					if (zaehler==gleiche) {
-						pair=true;
-					}
-					if (zaehler>gleiche) {
-						pair=false;
-					}
-				}
-			}
-			zaehler=0;
-			if (pair==true) { //damit z.B.: bei einem Full House bei der Suche nach OnePair die Funktion nicht fälschlicherwiese nur für Three of a Kind "true" zurückgibt
-				break;
-			}
-		}
-		return pair;
-	}
-	public static boolean twoPair() {
-		boolean twoPair=false;
-		int zaehler=0;
-		int paare=0;
-		int alteKarte=ANZAHL_KARTEN_PRO_FARBE;				//Sicherstellung, dass zaehler beim ersten Schleifendurchlauf reseted wird
-		for (int i=0;i<hANZAHL;i++) {
-			for (int j=(i+1);j<hANZAHL;j++) {
-				if (kartensymbolik(hand[i])==kartensymbolik(hand[j])) {
-					int neueKarte=kartensymbolik(hand[i]);
-					if (alteKarte!=neueKarte) {
-						if (zaehler<2) {
-							paare++;
-						}
-						if (zaehler==2) {
-							paare--;
-						}
-						zaehler=0;
-						alteKarte=neueKarte;
-					}
+	
+	//KOMBINATIONEN:
+	
+	public static boolean checkPair(int amountDuplicates) {
+		int zaehler = 0;
+		for (int i = 0; i < (hANZAHL - 1); i++) {
+			for (int j = (i + 1); j < hANZAHL; j++) {
+				if (kartensymbolik(hand[i]) == kartensymbolik(hand[j])) {
 					zaehler++;
 				}
 			}
 		}
-		if (paare==2) {
-			twoPair=true;
+		if (zaehler == amountDuplicates) {
+			return true;
+		}
+		return false;
+	}
+	/*public static boolean twoPair() {
+		boolean twoPair = false;
+		int zaehler_paare = 0;
+		int alteKarte = ANZAHL_KARTEN_PRO_FARBE;				//Sicherstellung, dass das erste Paar schon gewertet wird
+		for (int i = 0; i < hANZAHL - 1; i++) {
+			for (int j = (i + 1); j < hANZAHL; j++) {
+				if (kartensymbolik(hand[i]) == kartensymbolik(hand[j])) {
+					int neueKarte = kartensymbolik(hand[i]);
+					if (alteKarte != neueKarte) {
+						zaehler_paare++;
+						alteKarte = neueKarte;
+					}
+				}
+			}
+		}
+		if (zaehler_paare == 2) {
+			twoPair = true;
 		}
 		return twoPair;
-	}
+	}*/
 	public static boolean flush() {
-		boolean flush=true;
-		int farbeHand=farbe(hand[0]);
-		for (int i=1;i<hANZAHL;i++) {
-			if(farbe(hand[i])!=farbeHand) {
-				flush=false;
+		int farbeHand = farbe(hand[0]);
+		for (int i = 1; i < hANZAHL; i++) {
+			if(farbe(hand[i]) != farbeHand) {
+				return false;
 			}
 		}
-		return flush;
+		return true;
 	}
 	public static boolean straight() {
-		boolean straight=true;
-		int[] handSort=new int[hANZAHL];
-		for (int i=0;i<hANZAHL;i++) {
-			handSort[i]=kartensymbolik(hand[i]);
+		boolean straight = true;
+		int[] handSort = new int[hANZAHL];
+		for (int i = 0; i < hANZAHL; i++) {
+			handSort[i] = kartensymbolik(hand[i]);
 		}
 		Arrays.sort(handSort);
-		int letzteKarte=handSort[0];
-		for(int i=1;i<hANZAHL;i++) {
-			if(handSort[i]!=letzteKarte+1) {
-				straight=false;
+		int letzteKarte = handSort[0];
+		for(int i = 1; i < hANZAHL; i++) {
+			if(handSort[i] != letzteKarte + 1) {
+				straight = false;
 			}
-			letzteKarte=handSort[i];
+			letzteKarte = handSort[i];
+		}
+		if (handSort[0] == 0 && handSort[1] == 1 && handSort[2] == 2 && handSort[3] == 3 && handSort[4] == 12) {
+			straight = true;
 		}
 		return straight;
 	}
 	public static boolean royalFlush() {
-		boolean royalFlush=false;
-		int[] handSort=new int[hANZAHL];
-		for (int i=0;i<hANZAHL;i++) {
-			handSort[i]=kartensymbolik(hand[i]);
+		int[] handSort = new int[hANZAHL];
+		for (int i = 0; i < hANZAHL; i++) {
+			handSort[i] = kartensymbolik(hand[i]);
 		}
 		Arrays.sort(handSort);
-		if (straight() && flush() && handSort[hANZAHL-1]==12) {
-			royalFlush=true;
+		if (straight() && flush() && handSort[hANZAHL - 1] == 12) {
+			return true;
 		}
-		return royalFlush;
+		return false;
 	}
-	public static void wslAusgabe(String kombi, float zaehler) {
-		float wsl = (zaehler / (float)ANZAHL_DURCHLAEUFE) * 100;
+	
+	//WAHRSCHEINLICHKEITS-BERECHNUNG:
+	
+	public static void wsl(String kombination, float wslZaehler, String wslWikipedia) {
+		float wsl = wslZaehler * 100 / (float)anzahlDurchlaeufe;
+		//float wsl = wslZaehler * 100 / sumZaehler;
 		String s = String.format("%f", wsl); //Festkommadarstellung
-		System.out.println("Wahrscheinlichkeit für "+kombi+": "+s+"%");
-		//System.out.println(zaehler);
+		System.out.println("Wahrscheinlichkeit für " + kombination + ": " + s + "%");
+		System.out.println("\tWikipedia sagt " + wslWikipedia + "%");
+		//System.out.println(wslZaehler);
 		System.out.println();
 	}
 }
